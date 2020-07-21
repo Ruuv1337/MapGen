c1dac11a58f72c35faee897a780bf0d131e98780
[object Object] <31353526+Ruuv1337@users.noreply.github.com>
2020-07-21 05:19:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-var data = require('../../data/data.json'),
-      path = require('path'),
-      fs = require('fs'),
-      os = require('os');
+module.exports = function MAPGEN(mod) {}
 
-module.exports = function MAPGEN(mod) {
-    let NewData = '';
-    let MapsData = data.maps;
-    
-    for (let protocol in MapsData) {
-        for (let hook in MapsData[protocol]) {
-            NewData += hook+' = '+MapsData[protocol][hook]+os.EOL
-        }
-        
-        fs.writeFile(path.join(__dirname, protocol+'.map'), NewData, () => {
-            console.log('[MAP GEN] Generated a new protocol = '+protocol)
-        });
-        
-        NewData = '';
+var data = require('../../data/data.json'),
+    path = require('path'),
+    fs = require('fs'),
+    os = require('os');
+let NewData = '';
+let MapsData = data.maps;
+for (let protocol in MapsData) {
+    for (let hook in MapsData[protocol]) {
+        NewData += hook + ' = ' + MapsData[protocol][hook] + os.EOL
     }
+    fs.writeFile(path.join(__dirname, protocol + '.map'), NewData, () => {
+        console.log('[MAP GEN] Generated a new protocol = ' + protocol)
+    });
+    NewData = '';
 }
